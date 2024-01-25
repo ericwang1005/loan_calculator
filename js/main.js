6eb7f078c59e192445d357b44a5d6041f26ead27
Eric Wang <ew.961005@gmail.com>
2024-01-25 20:32:38 +0800

    '完成初步CSS'

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const amountEl = document.querySelector('#amount');
+const yearEl = document.querySelector('#year');
+const rateEl = document.querySelector('#rate');
+const feeEl = document.querySelector('#fee');
+const payment1El = document.querySelector('#payment1');
+const payment2El = document.querySelector('#payment2');
+
+console.log(amountEl,yearEl,rateEl,feeEl,payment1El,payment2El);
