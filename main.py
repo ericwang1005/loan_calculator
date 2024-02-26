3a4aedbd68d21d6daefdb104343576c3929533e7
Xuan Wang <xuan_wang2@edwards.com>
2024-02-26 14:43:18 +0800

    Add Items to Flask

---
+++ main.py
@@ -0,0 +1,10 @@
+# save this as app.py
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+app.run(debug='True')
